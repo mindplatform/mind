ff19b1021b96e8c08e7705fab5aee0a5d4c8ee69
0xReese <102135854+0xreese@users.noreply.github.com>
2025-02-14 19:42:10 +0800

    feat: add qstash dependency

--- packages/api/src/env.ts
+++ packages/api/src/env.ts
@@ -9,6 +9,7 @@ export const env = createEnv({
     S3_REGION: z.string().min(1),
     S3_ACCESS_KEY_ID: z.string().min(1),
     S3_SECRET_ACCESS_KEY: z.string().min(1),
+    UPSTASH_QSTASH_TOKEN: z.string().min(1),
     NODE_ENV: z.enum(['development', 'production']).optional(),
   },
   client: {},
